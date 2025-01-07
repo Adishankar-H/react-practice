45d93f68af43ccf500f5cbd824cf00c5dd54f8e8
Adishankar-H <official.adishankar@gmail.com>
2025-01-07 17:35:50 +0530

    13-tut commit successful...

---
+++ react_tuts_dg/13-tut/src/App.jsx
@@ -0,0 +1,102 @@
+import './App.css'
+import Header from './Header.jsx'
+import SearchItem from './SearchItem.jsx'
+import AddItem from './AddItem.jsx'
+import Content from './Content.jsx'
+import Footer from './Footer.jsx'
+import { useState, useEffect } from 'react'
+
+function App() {
+
+  const API_URL = "http://localhost:3500/items";
+
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error("Failed to fetch items");
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 2000)
+
+  }, [])
+
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const newItem = { id, checked: false, item };
+    const listItems = [...items, newItem];
+    setItems(listItems);
+  }
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+  }
+
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem('');
+  }
+
+  return (
+    <>
+      <Header
+        title="Grocery List"
+      />
+
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
+
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
+
+      <Footer
+        length={items.length}
+      />
+
+    </>
+  )
+}
+
+export default App
