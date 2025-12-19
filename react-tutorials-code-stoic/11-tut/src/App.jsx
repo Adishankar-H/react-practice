bb369dcc3293000ef4835a8b03e8d782de3c3482
Adishankar10 <offcial.adishankar@gmail.com>
2025-12-19 23:02:57 +0530

    state basics....

---
+++ react-tutorials-code-stoic/11-tut/src/App.jsx
@@ -0,0 +1,17 @@
+import ConditionalComponent from "./components/ConditionalComponent";
+import Fruits from "./components/Fruits";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      {/* <Fruits /> */}
+      {/* <ConditionalComponent /> */}
+      {/* <Message /> */}
+      <Counter />
+    </>
+  );
+}
+
+export default App;
